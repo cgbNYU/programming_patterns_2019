7264df526136b54f88cbbccb895f1e0a8df8267d
meisteronly <chapinbr@gmail.com>
2019-03-03 13:13:20 -0500

    Added Singleton logic to enemyManager so we never get two of them by accident.
    Might need to make it destroy itself on load in the future, but keep it around for now.

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+
+    public EnemyManager enemyManager;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (EnemyManager.instance == null)
+        {
+            Instantiate(enemyManager);
+        }
+    }
+}
