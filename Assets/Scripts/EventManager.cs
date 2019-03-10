9667c0688142ac91bf79e67f7b3c607044b7f8fb
meisteronly <chapinbr@gmail.com>
2019-03-10 14:49:16 -0400

    added using System to the EventManager added comments to each section of EventManager

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -1,25 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 // Large parts taken from Tim Miller's post on Type Safe Events in Unity 3D: http://www.willrmiller.com/?p=87
-public class GameEvent{}
+public class GameEvent{} //This just creates the Type GameEvent to be used in the system
 
 public class EventManager
 {
+    //These are the delegates that will hold the info about events within the dictionaries below
     public delegate void EventDelegate<T> (T e) where T : GameEvent;
     private delegate void EventDelegate (GameEvent e);
 
-    private readonly Dictionary<System.Type, EventDelegate> _delegates = new Dictionary<System.Type, EventDelegate>();
-    private readonly Dictionary<System.Delegate, EventDelegate> _delegateLookup = new Dictionary<System.Delegate, EventDelegate>();
+    //These take all of the Handlers as they get added and are used to pass the delegates to the handlers
+    private readonly Dictionary<Type, EventDelegate> _delegates = new Dictionary<System.Type, EventDelegate>();
+    private readonly Dictionary<Delegate, EventDelegate> _delegateLookup = new Dictionary<System.Delegate, EventDelegate>();
     private readonly List<GameEvent> _queuedEvents = new List<GameEvent>();
     private readonly object _queueLock = new object();
 
+    //Singleton stuff
     private static readonly EventManager _instance = new EventManager();
     public static EventManager Instance => _instance;
 
     private EventManager() {}
 
+    //When a piece of code needs to read an event, it adds a handler here.
     public void AddHandler<T> (EventDelegate<T> del) where T : GameEvent
     {
         if (_delegateLookup.ContainsKey(del))
@@ -38,6 +43,8 @@ public class EventManager
         }
     }
 
+    //This removes the handlers when the piece of code no longer needs them
+    //MAKE SURE TO DO THIS
     public void RemoveHandler<T>(EventDelegate<T> del) where T : GameEvent
     {
         EventDelegate internalDelegate;
@@ -55,6 +62,7 @@ public class EventManager
         }
     }
 
+    //Clears the dictionaries when they are empty
     public void Clear()
     {
         lock (_queueLock)
@@ -65,6 +73,7 @@ public class EventManager
         }
     }
 
+    //This launches the handler of the given delegate
     public void Fire(GameEvent e)
     {
         EventDelegate del;
@@ -74,6 +83,7 @@ public class EventManager
         }
     }
 
+    //This is if we have asynchronous events and will work like Fire
     public void ProcessQueuedEvents()
     {
         List<GameEvent> events;
@@ -96,6 +106,8 @@ public class EventManager
         }
     }
 
+    //Adds delegates to the queue as they come in
+    //These delegates are then processed by ProcessQueuedEvents
     public void Queue(GameEvent e)
     {
         lock (_queueLock)
